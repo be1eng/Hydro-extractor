e7a5f3909e5f991dfdaf03631cae5abd8bd0514b
Be1en <valeria.godoy@tecsup.edu.pe>
2025-06-19 16:04:08 +0000

    Actualizacion de opciones de Chrome

--- main.py
+++ main.py
@@ -57,11 +57,17 @@ variable_opcion = "C"
 
 # ------------------------------------------Configuración de Chrome------------------------------------------
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")  # Usa el nuevo modo headless (desde Chrome 109+)
+chrome_options.add_argument("--headless=new")  # Desde Chrome 109+
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-software-rasterizer")
+chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--disable-background-networking")
+
 
 # Inicia el navegador en segundo plano
 browser = webdriver.Chrome(options=chrome_options)
